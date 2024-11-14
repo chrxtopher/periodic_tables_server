745cf59d495d431a4c78568db631e8a452db3445
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2024-11-14 10:51:15 -0700

    removed comments

--- database/migrations/20241114173330_createReservationsTable.js
+++ database/migrations/20241114173330_createReservationsTable.js
@@ -1,7 +1,3 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.up = function (knex) {
   return knex.schema.createTable("reservations", (table) => {
     table.increments("reservation_id").primary();
@@ -15,10 +11,6 @@ exports.up = function (knex) {
   });
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.down = function (knex) {
   return knex.schema.dropTable("reservations");
 };
