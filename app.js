cecc323a90b6ccf83785ef86a45b6f42c4692171
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2024-11-14 12:15:12 -0700

    implemented reservation functionality for api

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const cors = require("cors");
 
 const app = express();
 
+const reservationsRouter = require("./reservations/reservations.router");
+
 app.use(cors());
 app.use(express.json());
+
+app.use("/reservations", reservationsRouter);
+
+app.use(errorHandler);
 
 module.exports = app;
