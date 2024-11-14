e1ccefae102e866edf196ed4817851c16ec52852
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2024-11-14 12:12:35 -0700

    route handler for /reservations

---
+++ reservations/reservations.router.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const controller = require("./reservations.controller");
+
+router.route("/").get(controller.list).post(controller.create);
+router.route("/:reservation_id").get(controller.read).put(controller.update);
+router.route("/:reservation_id/status").put(controller.updateStatus);
+
+module.exports = router;
