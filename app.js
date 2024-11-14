4361a7c57268ddffbde7e68a94097c0380faac64
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2024-11-14 12:23:42 -0700

    import error handler to express application

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const app = express();
 
 const reservationsRouter = require("./reservations/reservations.router");
+const errorHandler = require("./errors/errorHandler");
 
 app.use(cors());
 app.use(express.json());
