1a8e8097b140675eb9648807121397ec3b47904e
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2024-11-15 00:24:36 -0700

    service file that build SQL queries for the tables table

---
+++ tables/tables.service.js
@@ -0,0 +1,49 @@
+const knex = require("../database/knex");
+
+const create = async (table) => {
+  return knex("tables")
+    .insert(table)
+    .returning("*")
+    .then((newRecords) => newRecords[0]);
+};
+
+const read = (table_id) => {
+  return knex("tables").select("*").where({ table_id }).first();
+};
+
+const seatTable = async (table_id, reservation_id) => {
+  return knex("reservations")
+    .where({ reservation_id })
+    .update({ status: "seated" })
+    .then(() => {
+      return knex("tables")
+        .where({ table_id })
+        .update({ reservation_id })
+        .returning("*");
+    });
+};
+
+const clearTable = async (table_id, reservation_id) => {
+  return knex("reservations")
+    .where({ reservation_id })
+    .update({ status: "complete" })
+    .returning("*")
+    .then(() => {
+      return knex("tables")
+        .where({ table_id })
+        .update({ reservation_id: null })
+        .returning("*");
+    });
+};
+
+const list = () => {
+  return knex("tables").select("*").orderBy("table_name");
+};
+
+module.exports = {
+  create,
+  read,
+  seatTable,
+  clearTable,
+  list,
+};
