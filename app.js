af016664e8d038af982ac00a5405b75db35d7d72
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2024-11-15 00:26:08 -0700

    implemented tablesRouter in express application

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const cors = require("cors");
 const app = express();
 
 const reservationsRouter = require("./reservations/reservations.router");
+const tablesRouter = require("./tables/tables.router");
 const errorHandler = require("./errors/errorHandler");
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/reservations", reservationsRouter);
+app.use("/tables", tablesRouter);
 
 app.use(errorHandler);
 
