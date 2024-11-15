30c6ae2c2b691d9627374757f7a4af89217bc67c
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2024-11-15 00:25:46 -0700

    api routing established for tables requests

---
+++ tables/tables.router.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const controller = require("./tables.controller");
+
+router.route("/").get(controller.list).post(controller.create);
+
+router.route("/:table_id").get(controller.read);
+
+router
+  .route("/:table_id/seat")
+  .put(controller.seatTable)
+  .delete(controller.clearTable);
+
+module.exports = router;
