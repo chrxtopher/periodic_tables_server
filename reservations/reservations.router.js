9f241fbcc1143081cf740299a0b86fd051f622b4
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2024-11-16 04:39:16 -0700

    router now handles selecting all complete reservations

--- reservations/reservations.router.js
+++ reservations/reservations.router.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const controller = require("./reservations.controller");
 
 router.route("/").get(controller.list).post(controller.create);
+router.route("/complete").get(controller.listCompleteReservations);
 router.route("/:reservation_id").get(controller.read).put(controller.update);
 router.route("/:reservation_id/status").put(controller.updateStatus);
 
