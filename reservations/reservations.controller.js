b9ea285cc1cd33572c053242d47ed0fa714f93e7
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2024-11-17 01:06:10 -0700

    Created extensive middleware for reservations endpoint
    this middleware validates data sent with a request

--- reservations/reservations.controller.js
+++ reservations/reservations.controller.js
@@ -6,7 +6,7 @@ const create = async (req, res) => {
   res.status(201).json({ data });
 };
 
-const read = async (req, res) => {
+const read = (req, res) => {
   const data = res.locals.reservation;
   res.status(200).json({ data });
 };
@@ -55,6 +55,19 @@ const listCompleteReservations = async (req, res, next) => {
 // VALIDATION MIDDLEWARE //
 ///////////////////////////
 
+const validateReqDataExists = (req, res, next) => {
+  const { data } = req.body;
+
+  if (!data) {
+    return next({
+      status: 400,
+      message: "request body is empty",
+    });
+  }
+
+  next();
+};
+
 const reservationExists = async (req, res, next) => {
   const reservation_id = Number(req.params.reservation_id);
   const reservation = await reservationsService.read(reservation_id);
@@ -70,14 +83,185 @@ const reservationExists = async (req, res, next) => {
   }
 };
 
+const validateName = (req, res, next) => {
+  // checks if first and last name is provided or blank
+  const {
+    data: { first_name },
+  } = req.body;
+
+  const {
+    data: { last_name },
+  } = req.body;
+
+  if (!first_name || first_name.replace(/\s+/g, "").length === 0) {
+    return next({
+      status: 400,
+      message: "first name is required",
+    });
+  }
+
+  if (!last_name || last_name.replace(/\s+/g, "").length === 0) {
+    return next({
+      status: 400,
+      message: "last name is required",
+    });
+  }
+
+  next();
+};
+
+const validatePeople = (req, res, next) => {
+  const {
+    data: { people },
+  } = req.body;
+
+  if (!people) {
+    return next({
+      status: 400,
+      message: "total party members is required to book reservation",
+    });
+  }
+
+  if (people < 1) {
+    return next({
+      status: 400,
+      message: "minimum one person per reservation",
+    });
+  }
+
+  if (typeof people !== "number") {
+    return next({
+      status: 400,
+      message: "people must be of data type integer",
+    });
+  }
+
+  next();
+};
+
+const validateReservationDate = (req, res, next) => {
+  // checks if date is provided or blank, of correct format, and not a day in the past
+  const today = new Date();
+  const requiredFormat = /\d\d\d\d-\d\d-\d\d/;
+  const {
+    data: { reservation_date, reservation_time },
+  } = req.body;
+
+  if (!reservation_date || reservation_date.replace(/\s+/g, "").length === 0) {
+    return next({
+      status: 400,
+      message: "reservation date is required",
+    });
+  }
+
+  if (!reservation_date.match(requiredFormat)) {
+    return next({
+      status: 400,
+      message: "incorrect date format :: YYYY-MM-DD required",
+    });
+  }
+
+  if (new Date(`${reservation_date} ${reservation_time}`) < today) {
+    return next({
+      status: 400,
+      message:
+        "Time travelling is fun, but we have no way to accommodate reservations of the past.",
+    });
+  }
+
+  next();
+};
+
+const validateReservationTime = (req, res, next) => {
+  // checks if reservation time is provided or blank, and between 10:30am - 9:30pm.
+  const {
+    data: { reservation_time },
+  } = req.body;
+
+  if (!reservation_time || reservation_time.replace(/\s+/g, "").length === 0) {
+    return next({
+      status: 400,
+      message: "reservation time is required",
+    });
+  }
+
+  if (reservation_time < "10:30:00" || reservation_time > "21:30:00") {
+    return next({
+      status: 400,
+      message:
+        "requested time is outside hours of operation => 10:30 A.M. - 09:30 P.M.",
+    });
+  }
+
+  next();
+};
+
+const validateReservationStatusPOST = (req, res, next) => {
+  const {
+    data: { status },
+  } = req.body;
+
+  if (status) {
+    return next({
+      status: 400,
+      message: "new reservations are not allowed to provide a status",
+    });
+  }
+
+  next();
+};
+
+const validateReservationStatusPUT = (req, res, next) => {
+  const validStatus = ["booked", "seated", "complete", "cancelled"];
+  const {
+    data: { status },
+  } = req.body;
+
+  if (!status) {
+    return next({
+      status: 400,
+      message: "status required when updating a reservation",
+    });
+  }
+
+  if (!validStatus.includes(status)) {
+    return next({
+      status: 400,
+      message:
+        "reservation status can only be [booked, seated, complete, or cancelled]",
+    });
+  }
+
+  next();
+};
+
 module.exports = {
   list: [asyncErrorBoundary(list)],
   listCompleteReservations: [asyncErrorBoundary(listCompleteReservations)],
-  create: [asyncErrorBoundary(create)],
-  read: [asyncErrorBoundary(reservationExists), asyncErrorBoundary(read)],
-  update: [asyncErrorBoundary(reservationExists), asyncErrorBoundary(update)],
-  updateStatus: [
+  create: [
+    validateReqDataExists,
+    validateReservationDate,
+    validateReservationTime,
+    validatePeople,
+    validateName,
+    validateReservationStatusPOST,
+    asyncErrorBoundary(create),
+  ],
+  read: [asyncErrorBoundary(reservationExists), read],
+  update: [
     asyncErrorBoundary(reservationExists),
+    validateReqDataExists,
+    validateReservationDate,
+    validateReservationTime,
+    validatePeople,
+    validateName,
+    validateReservationStatusPUT,
+    asyncErrorBoundary(update),
+  ],
+  updateStatus: [
+    validateReqDataExists,
+    asyncErrorBoundary(reservationExists),
+    validateReservationStatusPUT,
     asyncErrorBoundary(updateStatus),
   ],
 };
